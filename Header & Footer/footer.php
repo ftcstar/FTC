7a24d349f930d00609b8a66eac84079c194bcae9
varun <varunasid@gmail.com>
2016-02-17 23:49:17 +0530

    header and footer version-1.0 added

---
+++ Header & Footer/footer.php
@@ -0,0 +1,60 @@
+<footer class="footer-distributed">
+
+			<div class="footer-left">
+
+				<h3>FTC<span> Star</span></h3>
+
+				<p class="footer-links">
+					<a href="#">Home</a>
+					·
+					<a href="#">Movies</a>
+					·   
+					<a href="#">Fan Club</a> 
+					·
+					<a href="#">Cine-career</a>
+					·
+					<a href="#">About us</a>
+				</p>
+
+				<p class="footer-company-name">FTC Star &copy; 2016</p>
+			</div>
+
+			<div class="footer-center">
+
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>XXXXXXXXXXXXXXXX</span>xxxxxxxxxxxx</p>
+				</div>
+
+				<div>
+					<i class="fa fa-phone"></i>
+					<p>+91-XXXXXXXXX</p>
+				</div>
+
+				<div>
+					<i class="fa fa-envelope"></i>
+					<p><a href="mailto:support@ftcstar.com">support@ftcstar.com</a></p>
+				</div>
+
+			</div>
+
+			<div class="footer-right">
+
+				<p class="footer-company-about">
+					<span>About the company</span>
+					FTC Star - connects the fans with their favourite celebrities, provide hourly updates on movies of your favourite star.
+				</p>
+
+				<div class="footer-icons">
+
+					<a href="#"><i class="fa fa-facebook"></i></a>
+					<a href="#"><i class="fa fa-twitter"></i></a>
+					<a href="#"><i class="fa fa-linkedin"></i></a>
+					<a href="#"><i class="fa fa-youtube-square"></i></a>
+
+				</div>
+
+			</div>
+
+		</footer>
+</html>
---
+++ Header & Footer/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Second Bar Header</title>
+
+	<link rel="stylesheet" href="assets/demo.css">
+	<link rel="stylesheet" href="assets/header-second-bar.css">
+	<link rel="stylesheet" href="assets/footer-distributed-with-address-and-phones.css">
+	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+	<header class="header-two-bars">
+
+	<div class="header-first-bar">
+
+		<div class="header-limiter">
+
+			<h1><a href="#">FTC <span>Star</span></a></h1>
+
+			<a href="#" class="logout-button">Log-in</a>
+			<a href="#" class="logout-button">Sign-up</a>
+		</div>
+
+	</div>
+
+	<div class="header-second-bar">
+
+		<div class="header-limiter">
+			<nav>
+				<a href="#"><i class="fa fa-home"></i> Home</a>
+				<a href="#"><i class="fa fa-file-movie-o"></i> Movies</a>
+				<a href="#"><i class="fa fa-group"></i> Fans Club</a>
+				<a href="#"><i class="fa fa-cogs"></i> Cine-career</a>
+				<a href="#"><i class="fa fa-star-half-o"></i> About us</a>
+			</nav>
+
+		</div>
+
+	</div>
+
+    </header>
+</head>
