8e774caaf59d1c58e9e7d6652cde1a2b212b37dd
Nasir <naseer.speak@gmail.com>
2016-02-23 23:02:16 +0530

    updated header with toggle button

--- HeaderFooter/header.php
+++ HeaderFooter/header.php
@@ -15,8 +15,10 @@
 	<link rel="stylesheet" type="text/css" href="Style/style.css">
 	<link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
-	<header class="header-two-bars">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 
+	<header class="header-two-bars">
 	<div class="header-first-bar">
 
 		<div class="header-limiter">
@@ -30,25 +32,31 @@
 
 	</div>
 
-	<div class="header-second-bar">
-           
-			<form class="header-limiter navbar-form navbar-left">	
-              <input class="form-control" type="text" placeholder="Search...">
-              <input class="form-control" type="button" value="Search" name="Search"  />
-            </form>
-            
-		<div class="header-limiter">
-			<nav >
-				<a href="#"><i class="fa fa-home"></i> Home</a>
-				<a href="#"><i class="fa"></i> Movies</a>
-				<a href="#"><i class="fa"></i> Celebrities</a>
-				<a href="#"><i class="fa"></i> Fans Club</a>
-				<a href="#"><i class="fa"></i> Cine-career</a>
-				<a href="#"><i class="fa"></i> About us</a>
-			</nav>
-		</div> <!-- header-limiter -->
-
-	</div>
-
+        <nav class="navbar navbar-default">
+  			<div class="container-fluid">
+			    <div class="navbar-header">
+			      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span> 
+			      </button>
+			    </div>
+			    <div class="collapse navbar-collapse" id="myNavbar">
+			      <ul class="nav navbar-nav">
+			        <li class="active"><a href="#">Home</a></li>
+			        <li><a href="#">Actors</a></li>
+			        <li><a href="#">Movies</a></li> 
+			        <li><a href="#">Fan Club</a></li>
+			        <li><a href="#">About US</a></li> 
+			      </ul>
+			      <ul class="nav navbar-nav navbar-right">
+			        <form class="navbar-form navbar-left">	
+              			<input class="form-control" type="text" placeholder="Search...">
+		            	<input class="form-control" type="button" value="Search" name="Search"  />
+		            </form>
+			      </ul>
+			    </div>
+  			</div>
+		</nav>
     </header>
 </head>
