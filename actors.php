c71f43228f2762238a55dc852cc632b1304bb6f0
Nasir <naseer.speak@gmail.com>
2016-02-24 00:40:40 +0530

    add slider for home page

--- actors.php
+++ actors.php
@@ -25,10 +25,69 @@
   </head>
   <?php include("HeaderFooter/header.php");?>
   <body>
-  <br/>
     <div class="container">
 
-      <div class="jumbotron" style="height: 600px;"> body content goes here </div>
+      <div class="well" style="height: 1000px;"> 
+        <div class="row featurette">
+          <div class="col-md-5">
+            <h3 class="featurette-heading">Upcoming movies </h3>
+            <ul>
+              <li style="display: list-item;"> The Flash </li>
+              <li>The Vampire Diaries</li>
+              <li>Gotham</li>
+              <li>Game of Thrones</li>
+              <li>Breaking Bad</li>
+              <li>Friends</li>
+              <li>The BigBang Theory</li>
+            </ul>
+          </div>
+          <div class="col-md-7">
+
+                <div id="myCarousel" class="carousel slide" data-ride="carousel">
+                <!-- Indicators -->
+                <ol class="carousel-indicators">
+                  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#myCarousel" data-slide-to="1"></li>
+                  <li data-target="#myCarousel" data-slide-to="2"></li>
+                  <li data-target="#myCarousel" data-slide-to="3"></li>
+                </ol>
+
+                <!-- Wrapper for slides -->
+                <div class="carousel-inner" role="listbox">
+                  <div class="item active">
+                    <img src="gallery/home/images/img1.png" alt="Chania">
+                  </div>
+
+                  <div class="item">
+                    <img src="gallery/home/images/img2.png" alt="Chania">
+                  </div>
+
+                  <div class="item">
+                    <img src="gallery/home/images/img3.png" alt="Flower">
+                  </div>
+
+                  <div class="item">
+                    <img src="gallery/home/images/img4.png" alt="Flower">
+                  </div>
+                </div>
+
+                <!-- Left and right controls -->
+                <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+
+          </div>
+        </div>
+
+
+
+      </div>
 
     </div> <!-- /container -->
 
