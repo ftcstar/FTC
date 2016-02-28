ae9d06d20e0c3b0a22445f6f852e1b670bb0438d
Nasir <naseer.speak@gmail.com>
2016-02-28 11:50:38 +0530

    new commit

--- HeaderFooter/header.php
+++ HeaderFooter/header.php
@@ -73,7 +73,7 @@
 
                 <section class="popupBody">
                         <div class="social_login">
-                                <div class="">
+                                <!-- <div class="">
                                         <a href="#" class="social_box fb">
                                                 <span class="icon"><i class="fa fa-facebook"></i></span>
                                                 <span class="icon_title">Login with Facebook</span>
@@ -89,7 +89,7 @@
                                 <div class="centeredText">
                                         <span>Or use your Email address</span>
                                 </div>
-
+-->
                                 <div class="action_btns">
                                         <div class="one_half"><a href="#" id="login_form" class="btn">Login</a></div>
                                         <div class="one_half last"><a href="#" id="register_form" class="btn">Sign up</a></div>
