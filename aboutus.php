a20fc12bfef69161e0970bd5ab3e2c4ebd62bbed
Nasir <naseer.speak@gmail.com>
2016-02-28 13:46:04 +0530

    updated header and footer for aboutus page

---
+++ aboutus.php
@@ -0,0 +1,170 @@
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <style type="text/css">
+      #myBtn {
+          position: fixed;
+          font-size: 18px;
+          bottom: 8px;
+          right: 8px;
+      }
+    </style>
+
+    <title>About US</title>
+
+    <link rel="stylesheet" href="Style/demo.css">
+    <link rel="stylesheet" href="Style/header-second-bar.css">
+    <link rel="stylesheet" href="Style/footer-distributed-with-address-and-phones.css">
+
+    <link rel="stylesheet" href="Style/aboutus.css">
+
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    <link href='http://fonts.googleapis.com/css?family=Cookie' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+  </head>
+
+  <?php include("HeaderFooter/header.php");?>
+  <body>
+    <div class="container">
+
+      <div class="well" style="height: 600px;"> 
+       
+        <div class="row">
+            <div class="col-sm-7">
+              <h4 class="style5">About Us</h4>
+              <p class="style4" style="text-align:justify;width:auto;">We are concerned about movies promotions and connections of movies to a large number of people. By the permission movies producers only, we are promoting movies in online as well as in offline to reach the movies for more people.</p>
+              <p class="style4" style="text-align:justify;width:auto;">We had, at most respect for the fans unconditional love. For that we adopted some tactics and statergies to connect the fans to theire dream stars and we are putting our sincere efforts to reach this service to maximum fans. </p>
+              <p class="style4" style="text-align:justify;width:auto;">We are providing a platform for young artists to explore theire talents and also creating  awareness about piracy and its impact on film industry.</p>
+              <p class="style4" style="text-align:justify;width:auto;">&quot;Killing piracy is one of our main agenda&quot;. </p>
+          </div>  <!-- end col-sm-7 -->
+          <div class="col-sm-5">
+
+
+          <div id="myCarousel" class="carousel slide" data-ride="carousel">
+            <!-- Indicators -->
+            <ol class="carousel-indicators">
+              <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+              <li data-target="#myCarousel" data-slide-to="1"></li>
+              <li data-target="#myCarousel" data-slide-to="2"></li>
+              <li data-target="#myCarousel" data-slide-to="3"></li>
+            </ol>
+
+            <!-- Wrapper for slides -->
+            <div class="carousel-inner ftc_slider" role="listbox" style="box-shadow: 2px 2px 14px rgba(0, 0, 0, 0.25);;border-radius: 10px;border: 1px solid rgba(0, 0, 0, 0.15);overflow: hidden;margin: auto;display: inline-block;">
+              <div class="item active" >
+            <h4>Yesh1 Say's About Us</h4>
+            <img src="gallery/aboutus/Yash.JPG" alt="yesh" width="120" height="120">
+                  <p>Naveen Kumar Gowda (born 8 January 1986), known mononymously by his screen name Yash, is an Indian film actor who primarily works in Kannada films. Prior to films, he appeared in theater plays and television soaps.</p>
+              </div>
+
+              <div class="item" >
+                <h4>Yesh2 Say's About Us</h4>
+            <img src="gallery/aboutus/Yash.JPG" alt="yesh" width="120" height="120">
+                  <p>Naveen Kumar Gowda (born 8 January 1986), known mononymously by his screen name Yash, is an Indian film actor who primarily works in Kannada films. Prior to films, he appeared in theater plays and television soaps.</p>
+              </div>
+              <div class="item" >
+                <h4>Yesh3 Say's About Us</h4>
+            <img src="gallery/aboutus/Yash.JPG" alt="yesh" width="120" height="120">
+                  <p>Naveen Kumar Gowda (born 8 January 1986), known mononymously by his screen name Yash, is an Indian film actor who primarily works in Kannada films. Prior to films, he appeared in theater plays and television soaps.</p>
+              </div>
+              <div class="item">
+                <h4>Yesh4 Say's About Us</h4>
+            <img src="gallery/aboutus/Yash.JPG" alt="yesh" width="120" height="120">
+                  <p>Naveen Kumar Gowda (born 8 January 1986), known mononymously by his screen name Yash, is an Indian film actor who primarily works in Kannada films. Prior to films, he appeared in theater plays and television soaps.</p>
+              </div>
+              <div class="item">
+                <h4>Yesh5 Say's About Us</h4>
+            <img src="gallery/aboutus/Yash.JPG" alt="yesh" width="120" height="120">
+                  <p>Naveen Kumar Gowda (born 8 January 1986), known mononymously by his screen name Yash, is an Indian film actor who primarily works in Kannada films. Prior to films, he appeared in theater plays and television soaps.</p>
+              </div>
+              <div class="item">
+                <h4>Yesh6 Say's About Us</h4>
+            <img src="img/Yash.JPG" alt="yesh" width="120" height="120">
+                  <p>Naveen Kumar Gowda (born 8 January 1986), known mononymously by his screen name Yash, is an Indian film actor who primarily works in Kannada films. Prior to films, he appeared in theater plays and television soaps.</p>
+              </div>
+
+            </div>
+
+          </div>
+          </div>  <!-- end col-sm-5 -->
+      </div> <!-- end row -->
+
+        <!-- Trigger the modal with a button  -->
+          <!-- <button type="button" class="btn btn-default btn-lg" id="myBtn">Login</button> -->
+          <div style="position: absolute; left: 10px; top: 40px;border-radius:20px;" class="col-sm-6 ih-item circle effect2 left_to_right"> <a href="#">
+            <img src="gallery\aboutus\contact_us.ico" width="50" height="50" class="img-responsive"  id="myBtn" onClick="DoSomething();" style="border-radius:10px;box-shadow: 4px 4px 20px rgba(0, 0, 0, 0.75);"/></a>
+        </div>
+          <!-- Modal -->
+          <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog">
+            
+              <!-- Modal content-->
+              <div class="modal-content">
+                    <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal">&times;</button>
+                  <h4 class="modal-title">Contact Us</h4>
+                </div>
+                <!-- Container (Contact Section) -->
+            <div class="modal-body">
+        <div id="contact" class="container-fluid bg-grey">
+          
+          <div class="row">
+            <div class="col-sm-5">
+              <p>Contact us and we'll get back to you within 24 hours.</p>
+              <p><span class="glyphicon glyphicon-map-marker"></span> Bangalore, India</p>
+              <p><span class="glyphicon glyphicon-phone"></span> +91 99XXXXXXXX</p>
+              <p><span class="glyphicon glyphicon-envelope"></span> support@ftcstar.com</p>    
+            </div>
+            <div class="col-sm-7 slideanim">
+              <div class="row">
+                <div class="col-sm-6 form-group">
+                  <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+                </div>
+            <div class="col-sm-6 form-group">
+                  <input class="form-control" id="phone" name="phone" placeholder="Pnone No." type="text" required>
+                </div>
+                <div class="col-sm-6 form-group">
+                  <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+                </div>
+              </div>
+              <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
+              <div class="row">
+                <div class="col-sm-12 form-group">
+                  <button class="btn btn-default pull-right" type="submit">Submit</button>
+                </div>
+              </div>  
+            </div>
+          </div>
+        </div>
+
+            </div>
+          </div> 
+        </div>
+         </div>
+
+    </div>  <!-- /well -->
+
+   </div> <!-- /container -->
+
+</body>
+
+    <script>
+      $(document).ready(function(){
+          $("#myBtn").click(function(){
+              $("#myModal").modal();
+          });
+      });
+    </script>
+
+<?php include("HeaderFooter/footer.php"); ?>
+</html>
